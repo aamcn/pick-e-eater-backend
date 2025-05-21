23e06d78111cae20314ba2a7613e530a76f51b2f
malcomcrab <malcomcrabesq@outlook.com>
2025-05-21 14:00:28 +0100

    Perf: Rename script to populateDb.js and refactor script to create and populate 'meals' table as well.

--- db/populateDb.js
+++ db/populateDb.js
@@ -13,7 +13,24 @@ CREATE TABLE IF NOT EXISTS people (
 
 INSERT INTO people (name, likes, dislikes) 
 VALUES ('Tony', '{"1"}', '{"2"}');
+
+CREATE TABLE IF NOT EXISTS meals (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR ( 255 ),
+  type VARCHAR ( 255 ),
+  sub_type VARCHAR ( 255 ),
+  difficulty VARCHAR ( 255 )
+);
+
+INSERT INTO meals (name, type, sub_type, difficulty) 
+VALUES ('Borger', 'Fast Food', 'American', 'Easy');
+
+INSERT INTO meals (name, type, sub_type, difficulty) 
+VALUES ('Lasagne', 'Pasta', 'Italian', 'Moderate');
+
 `
+
+
 
 async function main() {
   console.log("seeding...");
