02d98ea8cd97159056d463dbdd04e78f0f31db80
malcomcrab <malcomcrabesq@outlook.com>
2025-06-02 18:54:59 +0100

    Build: Add a connection string to pool

--- db/pool.js
+++ db/pool.js
@@ -1,10 +1,6 @@
 const { Pool } = require("pg");
 
 module.exports = new Pool({
-  host: process.env.PGHOST,
-  user: process.env.PGUSER,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
+  connectionString: process.env.DATABSE_URL
 });
 
